ed8098f5d730d35a4ce96245fcf5dfe4dd7dfc9d
chikara <chikara.f.yoshida@gmail.com>
2018-11-28 10:42:47 +0900

    繰り返せるようにしました

--- sekigae.py
+++ sekigae.py
@@ -3,48 +3,51 @@ import random
 all_list = []
 with open("members.txt") as names:
     name_list = names.read().split("\n")
-print(name_list)
 
 
 tf = False
 # anotherlist = random.sample(TableA_list, len(TableA_list))
-
-while tf == False:
-    anotherlist = random.sample(name_list, len(name_list))
-    for i in range(len(name_list)):
-        if i != len(name_list) - 1:
-            nabor_list = [name_list[i - 1], name_list[i + 1]]
-        else:
-            nabor_list = [name_list[i - 1], name_list[0]]
-        num = anotherlist.index(name_list[i])
-        if num != len(name_list) - 1:
-            another_nabor_list = [anotherlist[num - 1], anotherlist[num + 1]]
-            if (nabor_list[0] in another_nabor_list) or (nabor_list[1] in another_nabor_list):
-                break
+while True:
+    tf = Falsey
+    while tf == False:
+        anotherlist = random.sample(name_list, len(name_list))
+        for i in range(len(name_list)):
+            if i != len(name_list) - 1:
+                nabor_list = [name_list[i - 1], name_list[i + 1]]
             else:
-                if i != len(name_list) - 1:
-                    continue
-                else:
-                    print(anotherlist)
-                    tf = True
+                nabor_list = [name_list[i - 1], name_list[0]]
+            num = anotherlist.index(name_list[i])
+            if num != len(name_list) - 1:
+                another_nabor_list = [anotherlist[num - 1], anotherlist[num + 1]]
+                if (nabor_list[0] in another_nabor_list) or (nabor_list[1] in another_nabor_list):
                     break
-        else:
-            another_nabor_list = [anotherlist[num - 1], anotherlist[0]]
-            if (nabor_list[0] in another_nabor_list) or (nabor_list[1] in another_nabor_list):
-                break
+                else:
+                    if i != len(name_list) - 1:
+                        continue
+                    else:
+                        tf = True
+                        break
             else:
-                if i != len(name_list):
-                    continue
-                else:
-                    print(anotherlist)
-                    tf = True
+                another_nabor_list = [anotherlist[num - 1], anotherlist[0]]
+                if (nabor_list[0] in another_nabor_list) or (nabor_list[1] in another_nabor_list):
                     break
-    if not tf:
-        continue
-Table_List_A = anotherlist[:6]
-Table_list_B = anotherlist[6:11]
-Table_List_C = anotherlist[11:]
+                else:
+                    if i != len(name_list):
+                        continue
+                    else:
+                        tf = True
+                        break
+        if not tf:
+            continue
+    Table_List_A = anotherlist[:6]
+    Table_list_B = anotherlist[6:11]
+    Table_List_C = anotherlist[11:]
 
-print(Table_List_A)
-print(Table_list_B)
-print(Table_List_C)
+    print(Table_List_A)
+    print(Table_list_B)
+    print(Table_List_C)
+
+    name_list = anotherlist
+    question = input('席替えを続けますか?(y/n)')
+    if question.lower() == "n":
+        break
